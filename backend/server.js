652d53b51d4f7ed5b8d757132e7af24a428c873b
sarvottam <kumarpurush2511@gmail.com>
2024-07-09 01:22:23 +0530

    completed the task hopefully

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,14 @@ import bodyParser from 'body-parser'
 import serverRoutes from './routes/serverRoutes.js'
 import connectToDb from './db/connectToDb.js'
 import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // building server ...
 const app = express();
@@ -16,11 +23,20 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000 ;
 
+
 // routes 
-app.get("/" ,(req,res)=>{
-    res.send("hello don't worry !")
-})
+// app.get("/" ,(req,res)=>{
+//     res.send("hello don't worry !")
+// })
+
 app.use("/servers" , serverRoutes);
+
+app.use(express.static(path.join(__dirname ,'..', "/frontend","dist")));
+
+app.get("*" , (req, res) =>{
+    res.sendFile(path.resolve(__dirname ,'..',"frontend" , "dist" , "index.html" ));
+})
+
 
 app.listen(PORT , ()=>{
     connectToDb();
